ec89bd344e63cdc69fe96733e1a381498dc5bce9
Andy Lawson <alawson89@gmail.com>
2021-02-09 16:03:10 -0800

    update to fetch method

--- src/main.js
+++ src/main.js
@@ -2,32 +2,24 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import WeatherService from './weather-service.js';
 
+function getElements(response) {
+  if (response.main) {
+    $('.showHumidity').text(`The humidity in ${response.name} is ${response.main.humidity}%`);
+    $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+  } else {
+    $('.showErrors').text(`There was an error: ${response.message}`);
+  }
+}
 
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
-    const city = $('#location').val();
-    $('#location').val("");
-
-    let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.API_KEY}`;
-
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
+    let city = $('#location').val();
+    clearFields();
+    WeatherService.getWeather(city)
+      .then(function(response) {
         getElements(response);
-      }
-    };
-
-    request.open("GET", url, true);
-    request.send();
-
-    function getElements(response) {
-      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-      $('.showCurentTemp').text(`The temperature will be a high of ${response.main.temp_max} degrees and a low of ${response.main.temp_min} degrees, with the current being ${response.main.temp}.`);
-      $('.showHiLoTemp').text(`High of ${Math.round(response.main.temp_max)} degrees and a low of ${Math.round(response.main.temp_min)} degrees.`);
-      $('.showWind').text(`The wind is blowing at ${response.wind.speed} mph.`);
-      $('.showClouds').text(`The sky today will be ${response.weather[0].description}.`);
-    }
+      });
   });
 });
