8e87a47576786518a2e8c2e75826ad243ebf4e96
Andy Lawson <alawson89@gmail.com>
2021-02-09 16:28:22 -0800

    update main

--- src/main.js
+++ src/main.js
@@ -4,22 +4,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import WeatherService from './weather-service.js';
 
-function getElements(response) {
-  if (response.main) {
-    $('.showHumidity').text(`The humidity in ${response.name} is ${response.main.humidity}%`);
-    $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
-  } else {
-    $('.showErrors').text(`There was an error: ${response.message}`);
-  }
+function clearFields() {
+  $('#location').val("");
+  $('.showErrors').text("");
+  $('.showHumidity').text("");
+  $('.showTemp').text("");
 }
 
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
     let city = $('#location').val();
     clearFields();
-    WeatherService.getWeather(city)
-      .then(function(response) {
-        getElements(response);
-      });
+    let promise = WeatherService.getWeather(city);
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Fahrenheit is ${Math.round(body.main.temp)} degrees.`);
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error}`);
+    });
   });
 });
